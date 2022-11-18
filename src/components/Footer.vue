0c8194f89e83277846d291f44388525553fb4995
Simone Venturi <simone.venturi5@studio.unibo.it>
2022-11-18 17:36:34 +0100

    feat: add social links menu in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,28 @@
 <template>
-  <div id="footer" class="p-4">
-    <footer class="text-light p-4">
-      <small>&copy; 2022, Brandname</small>
+  <div id="footer" class="p-3">
+    <footer class="text-light p-2 container">
+        <div class="row justify-content-between mt-3 mb-3">
+          <div class="col-6 col-lg-3">
+            <small>&copy; 2022, Brandname</small>
+          </div>
+          <div class="col-6 col-lg-4 row justify-content-center">
+            <div class="col row justify-content-center">
+              <a href="https://www.facebook.com" target="_blank" ><font-awesome-icon icon="fa-brands fa-facebook" /></a> 
+            </div>
+            <div class="col row justify-content-center">
+              <a href="https://www.twitter.com/" target="_blank" ><font-awesome-icon icon="fa-brands fa-twitter" /></a>
+            </div>
+            <div class="col row justify-content-center">
+              <a href="https://www.instagram.com/" target="_blank" ><font-awesome-icon icon="fa-brands fa-instagram" /></a>
+            </div>
+            <div class="col row justify-content-center">
+              <a href="https://www.pinterest.com/" target="_blank" ><font-awesome-icon icon="fa-brands fa-pinterest" /></a>
+            </div>
+            <div class="col row justify-content-center">
+              <a href="https://www.linkedin.com/" target="_blank" ><font-awesome-icon icon="fa-brands fa-linkedin" /></a>
+            </div>
+          </div>
+        </div>
     </footer>
   </div>
 </template>
@@ -12,5 +33,31 @@
 #footer {
   background-color: black;
   width: 100%;
+  position: fixed;
+  bottom: 0;
+}
+a, a:hover { color: inherit; }
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+  
+}
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+  
+}
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {
+  .svg-inline--fa{
+    height: 1.5em;
+  }
+}
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+  
+}
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+  
 }
 </style>
